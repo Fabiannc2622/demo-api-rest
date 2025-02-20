c6424e9ffa7deaebb5f3300f9e80cb8dbadb081e
fabiannc2622 <pt70559075@idat.pe>
2025-02-20 10:02:54 -0500

    Segundo

--- src/main/java/pe/edu/idat/demo_api_rest/service/iFundamentosService.java
+++ src/main/java/pe/edu/idat/demo_api_rest/service/iFundamentosService.java
@@ -1,4 +1,12 @@
 package pe.edu.idat.demo_api_rest.service;
 
+import java.util.List;
+
 public interface iFundamentosService {
+
+    String validarNumeroPrimo(int numero);
+
+    String calcularFactorial(int numero);
+
+    List<Integer> calcularFibonacci(int numero);
 }
